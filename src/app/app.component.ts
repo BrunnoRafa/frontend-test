ab19cb104111baa886e94ef5418e9d98ad48a459
BrunnoRafa <brunor_17@hotmail.com>
2019-04-04 23:14:45 -0300

    Commit inicial do projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { CabecalhoComponent } from './components/cabecalho.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './components/cabecalho.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabecalhoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
