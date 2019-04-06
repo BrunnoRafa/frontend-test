46adbf39b52cf3c6ad184963c67ff3c7a61ee10b
BrunnoRafa <brunor_17@hotmail.com>
2019-04-06 13:57:23 -0300

    adicionando nova biblioteca, fonts e realizando alteracoes no app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { Ng2InputMaskModule } from 'ng2-input-mask';
 
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho.component';
+import { FormTransacaoComponent } from './components/form-transacao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    FormTransacaoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    Ng2InputMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
